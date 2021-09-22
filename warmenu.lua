bc498731fcbd7f20c1b78b67b4d6fc15076d76ea
ali-exacute <alinikbakht.98ia@gmail.com>
2021-09-22 10:20:48 +0330

    Update for my new scripts
    Changes : 
    Holding scroll will now works like vMenu (it will get faster as you hold it longer)
    Default Colors to GTA Online colors
    Submenu can have different title from main menu
    Strings now can be longer than 99 characters (kinda)
    Button callback is now more specific : `left` | `right` | `select`
    InputButton now have minimum character argument
    Add resetSelectedOption() to ... reset the selected option

--- warmenu.lua
+++ warmenu.lua
@@ -1,14 +1,7 @@
+-- Modified version of WarMenu by Ali Exacute#2588
+-- Unmodified version : https://github.com/warxander/warmenu
 WarMenu = { }
 WarMenu.__index = WarMenu
-
--- Deprecated
-WarMenu.debug = false
-function WarMenu.SetDebugEnabled(enabled)
-end
-function WarMenu.IsDebugEnabled()
-	return false
-end
----
 
 local menus = { }
 local keys = { down = 187, up = 188, left = 189, right = 190, select = 191, back = 194 }
@@ -40,10 +33,10 @@ local defaultStyle = {
 	y = 0.025,
 	width = 0.23,
 	maxOptionCountOnScreen = 10,
-	titleColor = { 0, 0, 0, 255 },
-	titleBackgroundColor = { 245, 127, 23, 255 },
+	titleColor = { 255, 255, 255, 255 },
+	titleBackgroundColor = { 33, 81, 156, 255 },
 	titleBackgroundSprite = nil,
-	subTitleColor = { 245, 127, 23, 255 },
+	subTitleColor = { 27, 98, 191, 255 },
 	textColor = { 255, 255, 255, 255 },
 	subTextColor = { 189, 189, 189, 255 },
 	focusTextColor = { 0, 0, 0, 255 },
@@ -52,6 +45,15 @@ local defaultStyle = {
 	subTitleBackgroundColor = { 0, 0, 0, 255 },
 	buttonPressedSound = { name = 'SELECT', set = 'HUD_FRONTEND_DEFAULT_SOUNDSET' }, --https://pastebin.com/0neZdsZ5
 }
+
+local scrolled = 0
+local lastScrollTimes = {}
+for _, key in pairs({ keys.up, keys.down, keys.left, keys.right }) do
+	lastScrollTimes[key] = GetGameTimer()
+end
+local scrollIntervalDefault = 200
+
+scrollInterval = scrollIntervalDefault
 
 local function setMenuProperty(id, property, value)
 	if not id then
@@ -159,15 +161,29 @@ local function setTextParams(font, color, scale, center, shadow, alignRight, wra
 	SetTextWrap(wrapFrom, wrapTo)
 end
 
-local function getLinesCount(text, x, y)
+-- TODO maybe change how it is handled and make it find first space/enter and add from there
+local function addLongString(text)
+    for i = 100, string.len(text), 99 do
+        local subStr = string.sub(text, i, i + 99)
+        AddTextComponentSubstringPlayerName(subStr)
+    end
+end
+
+function getLinesCount(text, x, y)
 	BeginTextCommandLineCount('TWOSTRINGS')
-	AddTextComponentString(tostring(text))
+	AddTextComponentSubstringPlayerName(text)
+	if string.len(text) > 99 then
+		addLongString(text)
+	end
 	return EndTextCommandGetLineCount(x, y)
 end
 
 local function drawText(text, x, y)
 	BeginTextCommandDisplayText('TWOSTRINGS')
-	AddTextComponentString(tostring(text))
+	AddTextComponentSubstringPlayerName(text)
+	if string.len(text) > 99 then
+		addLongString(text)
+	end
 	EndTextCommandDisplayText(x, y)
 end
 
@@ -272,13 +288,13 @@ function WarMenu.CreateMenu(id, title, subTitle, style)
 	menus[id] = menu
 end
 
-function WarMenu.CreateSubMenu(id, parent, subTitle, style)
+function WarMenu.CreateSubMenu(id, parent, title, subTitle, style)
 	local parentMenu = menus[parent]
 	if not parentMenu then
 		return
 	end
 
-	WarMenu.CreateMenu(id, parentMenu.title, subTitle and string.upper(subTitle) or parentMenu.subTitle)
+	WarMenu.CreateMenu(id, title, subTitle and string.upper(subTitle) or title and string.upper(title))
 
 	local menu = menus[id]
 
@@ -299,9 +315,11 @@ function WarMenu.CurrentMenu()
 	return currentMenu and currentMenu.id or nil
 end
 
-function WarMenu.OpenMenu(id)
+function WarMenu.OpenMenu(id, noSound)
 	if id and menus[id] then
-		PlaySoundFrontend(-1, 'SELECT', 'HUD_FRONTEND_DEFAULT_SOUNDSET', true)
+		if not noSound then
+			PlaySoundFrontend(-1, 'SELECT', 'HUD_FRONTEND_DEFAULT_SOUNDSET', true)
+		end
 		setMenuVisible(id, true)
 	end
 end
@@ -335,7 +353,16 @@ function WarMenu.CloseMenu()
 	end
 end
 
-function WarMenu.ToolTip(text, width, flipHorizontal)
+function WarMenu.GoBack()
+	if not currentMenu then
+		return
+	end
+	
+	setMenuVisible(currentMenu.previousMenu, true)
+	PlaySoundFrontend(-1, 'BACK', 'HUD_FRONTEND_DEFAULT_SOUNDSET', true)
+end
+
+function WarMenu.ToolTip(text, width, heightOffset, flipHorizontal)
 	if not currentMenu then
 		return
 	end
@@ -357,8 +384,7 @@ function WarMenu.ToolTip(text, width, flipHorizontal)
 	local textX = x - (width / 2) + buttonTextXOffset
 	setTextParams(buttonFont, getStyleProperty('textColor'), buttonScale, false, true, false, textX, textX + width - (buttonTextYOffset * 2))
 	local linesCount = getLinesCount(text, textX, getStyleProperty('y'))
-
-	local height = GetTextScaleHeight(buttonScale, buttonFont) * (linesCount + 1) + buttonTextYOffset
+	local height = GetTextScaleHeight(buttonScale, buttonFont) * (linesCount + 1) + buttonTextYOffset + (heightOffset and tonumber(heightOffset) and heightOffset or 0.0)
 	local y = getStyleProperty('y') + titleHeight + (buttonHeight * currentIndex) + height / 2
 
 	drawRect(x, y, width, height, getStyleProperty('backgroundColor'))
@@ -380,9 +406,13 @@ function WarMenu.Button(text, subText)
 
 	if currentMenu.currentOption == optionCount then
 		if currentKey == keys.select then
-			pressed = true
+			pressed = 'select'
 			PlaySoundFrontend(-1, getStyleProperty('buttonPressedSound').name, getStyleProperty('buttonPressedSound').set, true)
-		elseif currentKey == keys.left or currentKey == keys.right then
+		elseif currentKey == keys.left then
+			pressed = 'left'
+			PlaySoundFrontend(-1, 'NAV_UP_DOWN', 'HUD_FRONTEND_DEFAULT_SOUNDSET', true)
+		elseif currentKey == keys.right then
+			pressed = 'right'
 			PlaySoundFrontend(-1, 'NAV_UP_DOWN', 'HUD_FRONTEND_DEFAULT_SOUNDSET', true)
 		end
 	end
@@ -410,7 +440,7 @@ function WarMenu.SpriteButton(text, dict, name, r, g, b, a)
 	return pressed
 end
 
-function WarMenu.InputButton(text, windowTitleEntry, defaultText, maxLength, subText)
+function WarMenu.InputButton(text, windowTitleEntry, defaultText, maxLength, minLength, subText)
 	if not currentMenu then
 		return
 	end
@@ -418,7 +448,7 @@ function WarMenu.InputButton(text, windowTitleEntry, defaultText, maxLength, sub
 	local pressed = WarMenu.Button(text, subText)
 	local inputText = nil
 
-	if pressed then
+	if pressed == 'select' or pressed == 'right' then
 		DisplayOnscreenKeyboard(1, windowTitleEntry or 'FMMC_MPM_NA', '', defaultText or '', '', '', '', maxLength or 255)
 
 		while true do
@@ -428,8 +458,14 @@ function WarMenu.InputButton(text, windowTitleEntry, defaultText, maxLength, sub
 			if status == 2 then
 				break
 			elseif status == 1 then
-				inputText = GetOnscreenKeyboardResult()
-				break
+				local result = GetOnscreenKeyboardResult()
+				if result:len() >= (minLength and minLength or 0) then
+					inputText = result
+					break
+				else
+					showNotification('You have to atleast enter '..minLength..' characters')
+					DisplayOnscreenKeyboard(1, windowTitleEntry or 'FMMC_MPM_NA', '', result, '', '', '', maxLength or 255)
+				end
 			end
 
 			Citizen.Wait(0)
@@ -446,10 +482,12 @@ function WarMenu.MenuButton(text, id, subText)
 
 	local pressed = WarMenu.Button(text, subText)
 
-	if pressed then
+	if pressed == 'select' or pressed == 'right' then
 		currentMenu.currentOption = optionCount
 		setMenuVisible(currentMenu.id, false)
 		setMenuVisible(id, true, true)
+	elseif pressed == 'left' then
+		WarMenu.CloseMenu()
 	end
 
 	return pressed
@@ -524,30 +562,56 @@ function WarMenu.Display()
 			drawSubTitle()
 
 			currentKey = nil
+			local currentTime = GetGameTimer()
 
-			if IsDisabledControlJustReleased(0, keys.down) then
-				PlaySoundFrontend(-1, 'NAV_UP_DOWN', 'HUD_FRONTEND_DEFAULT_SOUNDSET', true)
+			if scrolled > 35 then
+				scrollInterval = 50
+			elseif scrolled > 10 then
+				scrollInterval = 100
+			end
 
-				if currentMenu.currentOption < optionCount then
-					currentMenu.currentOption = currentMenu.currentOption + 1
-				else
-					currentMenu.currentOption = 1
+			if IsDisabledControlPressed(0, keys.down) then
+				if (currentTime - lastScrollTimes[keys.down]) > scrollInterval then
+					PlaySoundFrontend(-1, 'NAV_UP_DOWN', 'HUD_FRONTEND_DEFAULT_SOUNDSET', true)
+
+					if currentMenu.currentOption < optionCount then
+						currentMenu.currentOption = currentMenu.currentOption + 1
+					else
+						currentMenu.currentOption = 1
+					end
+
+					lastScrollTimes[keys.down] = currentTime
+					scrolled = scrolled + 1
 				end
-			elseif IsDisabledControlJustReleased(0, keys.up) then
-				PlaySoundFrontend(-1, 'NAV_UP_DOWN', 'HUD_FRONTEND_DEFAULT_SOUNDSET', true)
+			elseif IsDisabledControlPressed(0, keys.up) then
+				if (currentTime - lastScrollTimes[keys.up]) > scrollInterval then
 
-				if currentMenu.currentOption > 1 then
-					currentMenu.currentOption = currentMenu.currentOption - 1
-				else
-					currentMenu.currentOption = optionCount
+					PlaySoundFrontend(-1, 'NAV_UP_DOWN', 'HUD_FRONTEND_DEFAULT_SOUNDSET', true)
+
+					if currentMenu.currentOption > 1 then
+						currentMenu.currentOption = currentMenu.currentOption - 1
+					else
+						currentMenu.currentOption = optionCount
+					end
+
+					lastScrollTimes[keys.up] = currentTime
+					scrolled = scrolled + 1
 				end
-			elseif IsDisabledControlJustReleased(0, keys.left) then
-				currentKey = keys.left
-			elseif IsDisabledControlJustReleased(0, keys.right) then
-				currentKey = keys.right
-			elseif IsControlJustReleased(0, keys.select) then
+			elseif IsDisabledControlPressed(0, keys.left) then
+				if (currentTime - lastScrollTimes[keys.left]) > scrollInterval then
+					currentKey = keys.left
+					lastScrollTimes[keys.left] = currentTime
+					scrolled = scrolled + 1
+				end
+			elseif IsDisabledControlPressed(0, keys.right) then
+				if (currentTime - lastScrollTimes[keys.right]) > scrollInterval then
+					currentKey = keys.right
+					lastScrollTimes[keys.right] = currentTime
+					scrolled = scrolled + 1
+				end
+			elseif IsControlJustPressed(0, keys.select) then
 				currentKey = keys.select
-			elseif IsDisabledControlJustReleased(0, keys.back) then
+			elseif IsDisabledControlJustPressed(0, keys.back) then
 				if menus[currentMenu.previousMenu] then
 					setMenuVisible(currentMenu.previousMenu, true)
 					PlaySoundFrontend(-1, 'BACK', 'HUD_FRONTEND_DEFAULT_SOUNDSET', true)
@@ -555,6 +619,26 @@ function WarMenu.Display()
 					WarMenu.CloseMenu()
 				end
 			end
+
+			if IsDisabledControlJustReleased(0, keys.down) then
+				lastScrollTimes[keys.down] = 0
+				scrolled = 0
+				scrollInterval = scrollIntervalDefault
+			elseif IsDisabledControlJustReleased(0, keys.up) then
+				lastScrollTimes[keys.up] = 0
+				scrolled = 0
+				scrollInterval = scrollIntervalDefault
+			elseif IsDisabledControlJustReleased(0, keys.left) then
+				lastScrollTimes[keys.left] = 0
+				scrolled = 0
+				scrollInterval = scrollIntervalDefault
+			elseif IsDisabledControlJustReleased(0, keys.right) then
+				lastScrollTimes[keys.right] = 0
+				scrolled = 0
+				scrollInterval = scrollIntervalDefault
+			end
+
+
 
 			optionCount = 0
 		end
@@ -568,6 +652,14 @@ function WarMenu.CurrentOption()
 	end
 
 	return nil
+end
+
+function WarMenu.resetSelectedOption()
+	if not currentMenu then
+		return
+	end
+
+	currentMenu.currentOption = 1
 end
 
 function WarMenu.IsItemHovered()
